01d596ddf8b4efd34a0c81928713fcda3101c8a4
abochan123 <96906477+abochan123@users.noreply.github.com>
2022-01-23 10:30:03 +0100

    Initial Commit

---
+++ Essai 2/Essai_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Essai_2App.swift
+//  Essai 2
+//
+//  Created by sawako barberon on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Essai_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
